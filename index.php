d191189c296400495cca09ddd60ae4455a727edb
shreyansh <shreyansh.2015@vit.ac.in>
2017-09-07 00:04:39 +0530

    Create the Login page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPEhtml>
+<html>
+<head>
+    <title>E-billing</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/animate.css">
+    
+
+
+</head>
+<body> 
+    <div class="container">
+        <div class="row">
+            <div class="login_outter_div col-md-5 col-xs-12 col-sm-10 wow bounceInDown animted">
+                <div class="inner_div">
+                    <form>
+                        <div class="row">
+                            <img class="img-responsive user_img" src="img/user.png" alt="useer">
+                            <input type="text" name="user_name" class="input_area col-md-12" placeholder="Username"/>
+                            <input type="password" name="password" class="input_area col-md-12" placeholder="Password"/>
+                            <button type="submit" class="btn login_btn col-md-12"><b>Login</b></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+    </script>
+</body>
+</html>
